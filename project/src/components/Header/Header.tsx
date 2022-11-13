14db4a8a6507874ede56d9a15ead30361126570a
Anton Fomichev <89206134166@inbox.ru>
2022-11-13 20:37:20 +0300

    Add login page components

--- project/src/components/Header/Header.tsx
+++ project/src/components/Header/Header.tsx
@@ -1,6 +1,8 @@
-import { User } from '../../const';
+type HeaderProps = {
+  userEmail?: string;
+}
 
-export function Header(): JSX.Element {
+export function Header({ userEmail }: HeaderProps): JSX.Element {
   return (
     <header className="header">
       <div className="container">
@@ -10,21 +12,22 @@ export function Header(): JSX.Element {
               <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
             </a>
           </div>
-          <nav className="header__nav">
-            <ul className="header__nav-list">
-              <li className="header__nav-item user">
-                <div className="header__nav-profile">
-                  <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-                  <span className="header__user-name user__name">{User.Email}</span>
-                </div>
-              </li>
-              <li className="header__nav-item">
-                <a className="header__nav-link" href="#">
-                  <span className="header__signout">Sign out</span>
-                </a>
-              </li>
-            </ul>
-          </nav>
+          {Boolean(userEmail) &&
+            <nav className="header__nav">
+              <ul className="header__nav-list">
+                <li className="header__nav-item user">
+                  <div className="header__nav-profile">
+                    <div className="header__avatar-wrapper user__avatar-wrapper"></div>
+                    <span className="header__user-name user__name">{userEmail}</span>
+                  </div>
+                </li>
+                <li className="header__nav-item">
+                  <a className="header__nav-link" href="#">
+                    <span className="header__signout">Sign out</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>}
         </div>
       </div>
     </header>
--- project/src/pages/MainPage/MainPage.tsx
+++ project/src/pages/MainPage/MainPage.tsx
@@ -1,10 +1,11 @@
 import { CitiesCatalog } from '../../components/CitiesCatalog/CitiesCatalog';
 import { Header } from '../../components/Header/Header';
+import { User } from '../../const';
 
 export function MainPage(): JSX.Element {
   return (
     <div className='page page--gray page--main'>
-      <Header />
+      <Header userEmail={User.Email} />
       <CitiesCatalog />
     </div>
   );
